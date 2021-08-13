4834a4cef71b58801d0fa3ca4fe3169cf0b36b41
Сергей Полещук <sergv77@yandex.ru>
2021-08-13 11:29:20 +0300

    удаление файлов и директорий

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextField, TextAreaField, SubmitField, StringField
-from wtforms.validators import Required, DataRequired, InputRequired
-
-class ParsForm(FlaskForm):
-    text = TextAreaField(validators=[DataRequired()])
-    submit = SubmitField("Распознать")
