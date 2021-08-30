baea4ebef4156cfd5b76107b5802e9f014b65934
Сергей Полещук <sergv77@yandex.ru>
2021-08-30 11:58:46 +0300

    добавление spacy

--- app/settings.py
+++ app/settings.py
@@ -7,11 +7,11 @@ from flask import render_template, redirect, request, url_for
 
 
 ####################***SPACY LIB***####################
-# import spacy
-# from spacy.lang.ru.examples import sentences
-# from spacy.symbols import ORTH, LEMMA
-# from spacy.lang.ru import Russian
-# from spacy.tokens.doc import Doc
-# from spacy.vocab import Vocab
-# from spacy import displacy
-# import ru_core_news_lg
+import spacy
+from spacy.lang.ru.examples import sentences
+from spacy.symbols import ORTH, LEMMA
+from spacy.lang.ru import Russian
+from spacy.tokens.doc import Doc
+from spacy.vocab import Vocab
+from spacy import displacy
+import ru_core_news_lg
